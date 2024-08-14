ccf3d58f71d2622419bd83cd92ffe44475fba43b
Owen Caleb Smith Cerdas <o.smith.1@estudiantec.cr>
2024-08-13 23:27:49 -0600

    Add files via upload
    Se añadio configuracion para ingresar valores de matriz tanto manual como aleatorio 
    Final :)

--- matriz.cpp
+++ matriz.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+
 int filas;
 int columnas;
 int filas2;
@@ -11,28 +12,93 @@ int opcion2;
 void menu(){
     printf("\n");
     printf("\n");
-    cout << "Seleccione una opcion:" << endl;
-    cout << "1. Crear matriz" << endl;
-    cout << "2. Reiniciar matriz" << endl;
-    cout << "3. Salir del programa" << endl;
-    cout << "Ingrese la opcion deseada: " << endl;
+    std::cout << "Seleccione una opcion:" << endl;
+    std::cout << "1. Crear matriz" << endl;
+    std::cout << "2. Reiniciar matriz" << endl;
+    std::cout << "3. Salir del programa" << endl;
+    std::cout << "Ingrese la opcion deseada: " << endl;
     printf("\n");
     printf("\n");
-    cin >> opcion;
+    std::cin >> opcion;
 }
 void menu2()
 {
     printf("\n");
     printf("\n");
-    cout << "Seleccione una opcion:" << endl; 
-    cout << "1. Sumar Matriz" << endl;
-    cout << "2. Triangulacion Reflexiva" << endl;
-    cout << "3. Reiniciar matriz" << endl;
-    cout << "4. Salir" << endl;
-    cout << "Ingrese la opcion deseada: " << endl;
+    std::cout << "Seleccione una opcion:" << endl; 
+    std::cout << "1. Sumar Matriz" << endl;
+    std::cout << "2. Triangulacion Reflexiva" << endl;
+    std::cout << "3. Reiniciar matriz" << endl;
+    std::cout << "4. Salir" << endl;
+    std::cout << "Ingrese la opcion deseada: " << endl;
+    printf("\n");
+    printf("\n");
+    std::cin >> opcion;
+}
+void menu3()
+{
+    printf("\n");
+    printf("\n");
+    std::cout << "¿Como deseas crear la matriz?" << endl;
+    std::cout << "Seleccione una opcion:" << endl; 
+    std::cout << "1. Manual" << endl;
+    std::cout << "2. Aleatorio" << endl;
+    std::cout << "3.Salir" << endl ;
+    std::cout << "Ingrese la opcion deseada: " << endl;
     printf("\n");
     printf("\n");
     cin >> opcion;
+}
+void crearMatrizManual(int matriz[20][20], int filas, int columnas, int matriz2[20][20])
+{
+    int posicion;
+    cout << "Matriz1: " << endl;
+    cout << "Filas: " << filas << " Columnas: " << columnas << endl;
+    for (int i = 0; i < filas; i++)
+    {
+        printf("[ ");
+        for (int j = 0; j < columnas; j++)
+        {
+            cout << "Ingrese valor para la posicion [" << i << "][" << j <<"]" << endl;
+            cin >> posicion;
+            matriz[i][j] = posicion;
+            printf("%i", matriz[i][j]);
+            if (j + 1 < columnas) 
+            {
+                printf(" , ");
+            }
+            if (j + 1 == columnas) 
+            {
+                printf(" ]");
+                printf("\n");
+                printf("\n");
+            }
+        }
+    }
+cout << "Matriz2: " << endl;
+    cout << "Filas: " << filas2 << " Columnas: " << columnas2 << endl;
+    for (int i = 0; i < filas2; i++)
+    {
+        printf("[ ");
+        for (int j = 0; j < columnas2; j++)
+        {
+            cout << "Ingrese valor para la posicion [" << i << "][" << j <<"]" << endl;
+            cin >> posicion;
+            matriz2[i][j] = posicion;
+            printf("%i", matriz2[i][j]);
+            if (j + 1 < columnas2) 
+            {
+                printf(" , ");
+            }
+            if (j + 1 == columnas2) 
+            {
+                printf(" ]");
+                printf("\n");
+                printf("\n");
+            }
+        }
+    }
+
 }
 void crearMatriz(int matriz[20][20], int filas, int columnas, int matriz2[20][20]) 
 {
@@ -132,6 +198,7 @@ void reflexiva(int matriz[20][20], int matriz2[20][20], int filas, int columnas)
     }
 }
 int main() {
+    bool bandera2 = false;
     bool reiniciar = false;
     bool bandera = false;
     //while (reiniciar != true){
@@ -179,7 +246,29 @@ int main() {
                 {
                 case 1:
                     // Crear dos matrices
-                    crearMatriz(matriz, filas, columnas, matriz2);
+                    do
+                    {   
+                        menu3();
+                        switch(opcion)
+                        {
+                            case 1:
+                                crearMatrizManual(matriz, filas, columnas, matriz2);
+                                bandera2 = true;
+                                //return 0;
+                                break;
+                            case 2:
+                                crearMatriz(matriz, filas, columnas, matriz2);
+                                bandera2  = true;
+                                //return 0;
+                                break;
+                            case 3:
+                                cout << "Se salio del programa" << endl;
+                                return 0;
+                                break;
+
+                        }
+
+                    }while(bandera2 == false);
                     bandera = true;
                     break;
 
@@ -213,16 +302,15 @@ int main() {
                             break;
                         case 3:
                             cout << "Reiniciar Matrices" << endl;
-                           
                             main();
                             break;
                         case 4:
                             cout << "Se salio" << endl;
-                            bandera = false;
+                            return 0;
                             break;
                         default: 
                             cout << "Entrada Invalida, por favor ingrese el numero correspondiente a la opcion deseada." << endl;
-                                menu2();
+                            menu2();
                             break;
                 }
             }while (bandera);
