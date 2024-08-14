adf65b98014141e111247f04d235ba411d457935
Owen Caleb Smith Cerdas <o.smith.1@estudiantec.cr>
2024-08-13 22:47:27 -0600

    Add files via upload
    --Modificacion de bucles
    --Modificacion de menus
    --Finalizacion

--- matriz.cpp
+++ matriz.cpp
@@ -6,88 +6,100 @@ int filas2;
 int columnas2;
 int mat;
 int opcion;
-    void menu(){
-        cout << "Seleccione una opcion:" << endl;
-        cout << "1. Crear matriz" << endl;
-        cout << "2. Sumar Matriz" << endl;
-        cout << "3. Triangulacion Reflexiva" << endl;
-        cout << "4. Reiniciar matriz" << endl;
-        cout << "5. Salir del programa" << endl;
-        cout << "Ingrese la opcion deseada: " << endl;
-        printf("\n");
-        printf("\n");
-        cin >> opcion;
-        }
-void crearMatriz(int matriz[22][22],int filas,int columnas,int matriz2[22][22]) 
+int opcion2;
+void menu(){
+    printf("\n");
+    printf("\n");
+    cout << "Seleccione una opcion:" << endl;
+    cout << "1. Crear matriz" << endl;
+    cout << "2. Reiniciar matriz" << endl;
+    cout << "3. Salir del programa" << endl;
+    cout << "Ingrese la opcion deseada: " << endl;
+    printf("\n");
+    printf("\n");
+    cin >> opcion;
+}
+void menu2()
+{
+    printf("\n");
+    printf("\n");
+    cout << "Seleccione una opcion:" << endl; 
+    cout << "1. Sumar Matriz" << endl;
+    cout << "2. Triangulacion Reflexiva" << endl;
+    cout << "3. Reiniciar matriz" << endl;
+    cout << "4. Salir" << endl;
+    cout << "Ingrese la opcion deseada: " << endl;
+    printf("\n");
+    printf("\n");
+    cin >> opcion;
+}
+void crearMatriz(int matriz[20][20], int filas, int columnas, int matriz2[20][20]) 
 {
     int cont =  1;
     int cont2 = 1;
     cout << "Matriz1: "<< endl;
-    cout << "Filas" << filas << "Columnas: " << columnas << endl;
+    cout << "Filas: " << filas << " Columnas: " << columnas << endl;
     printf("\n");
-    for (int i =0; i<filas ;i++)
+    for (int i = 0; i < filas; i++)
     {
         printf("[ ");
-        for (int j=0;j<columnas;j++)
+        for (int j = 0; j < columnas; j++)
         {
             matriz[i][j] = cont;
-            cont +=1;
-            printf("%i",matriz[i][j]);
-            if (j+1 < columnas){
+            cont += 1;
+            printf("%i", matriz[i][j]);
+            if (j + 1 < columnas) {
                 printf(" , ");
             }
-            if (j+1 == columnas)
-            {
-            printf(" ]");
-            printf("\n");
-            printf("\n");
+            if (j + 1 == columnas) {
+                printf(" ]");
+                printf("\n");
+                printf("\n");
             }
         }
     }
     cout << "Matriz2: "<< endl;
-    cout << "Filas" << filas2 << "Columnas: " << columnas2 << endl;
+    cout << "Filas: " << filas2 << " Columnas: " << columnas2 << endl;
     printf("\n");
-    for (int i =0; i<filas2 ;i++)
+    for (int i = 0; i < filas2; i++)
     {
         printf("[ ");
-        for (int j=0;j<columnas2;j++)
+        for (int j = 0; j < columnas2; j++)
         {
             matriz2[i][j] = cont - cont2;
-            cont2 +=1;
-            printf("%i",matriz2[i][j]);
-            if (j+1 < columnas2){
+            cont2 += 1;
+            printf("%i", matriz2[i][j]);
+            if (j + 1 < columnas2) {
                 printf(" , ");
             }
-            if (j+1 == columnas2)
-            {
-            printf(" ]");
-            printf("\n");
-            printf("\n");
+            if (j + 1 == columnas2) {
+                printf(" ]");
+                printf("\n");
+                printf("\n");
             }
         }
     }
 }
-void sumar_matriz(int matriz[22][22],int matriz2[22][22],int matrizres[22][22],int filas,int columnas){
+void sumar_matriz(int matriz[20][20], int matriz2[20][20],  int matrizres[20][20], int filas, int columnas){
     if (filas == filas2 && columnas == columnas2)
     {
         cout << "Matriz Resultado: " << endl;
-        for (int i = 0;i<filas;i++)
+        for (int i = 0; i < filas; i++)
         {
             printf("[ ");
-            for (int j = 0;j<columnas;j++)
+            for (int j = 0; j < columnas; j++)
             {
                 matrizres[i][j] = matriz[i][j] + matriz2[i][j];
-
-                printf("%i",matrizres[i][j]);
-                if (j+1 < columnas)
+                printf("%i", matrizres[i][j]);
+                if (j + 1 < columnas)
                 {
                     printf(" , ");
                 }
-                if (j+1 == columnas)
+                if (j + 1 == columnas)
                 {
-                printf(" ]");
-                printf("\n");
-                printf("\n");
+                    printf(" ]");
+                    printf("\n");
+                    printf("\n");
                 }
             }
         }
@@ -96,14 +108,14 @@ void sumar_matriz(int matriz[22][22],int matriz2[22][22],int matrizres[22][22],i
         cout << "No se cumplen las condiciones para sumar" << endl;
     }
 }
-void reflexiva(int matriz[22][22],int matriz2[22][22],int filas,int columnas)
+void reflexiva(int matriz[20][20], int matriz2[20][20], int filas, int columnas)
 {
     bool reflex = true;
-    for (int i=0;i<filas;i++)
+    for (int i = 0; i < filas; i++)
     {
-        for (int j=0;j<columnas;j++)
+        for (int j = 0; j < columnas; j++)
         {
-            if (matriz[i][j] != matriz2[(filas-i)-1][(columnas-j)-1])
+            if (matriz[i][j] != matriz2[(filas - i) - 1][(columnas - j) - 1])
             {
                 reflex = false;
             }
@@ -120,54 +132,99 @@ void reflexiva(int matriz[22][22],int matriz2[22][22],int filas,int columnas)
 }
 int main() {
     bool reiniciar = false;
-    while (reiniciar != true){
-        cout << "Ingrese las dimensiones de la matriz_1 desada: " << endl;
-        cout <<"Filas: " << endl;
-        cin >> filas;
-        cout << "" << endl;
-        cout << "Columnas: " << endl;
-        cin >> columnas;
-        cout << "" << endl;
-        int matriz[filas][columnas];
+    bool bandera = false;
+    //while (reiniciar != true){
+        do {
+            cout << "Ingrese las dimensiones de la matriz_1 deseada: " << endl;
+            cout << "Filas (2-20): " << endl;
+            cin >> filas;
+            if (filas < 2 || filas > 20) {
+                cout << "Valor invalido. Las filas deben estar entre 2 y 20." << endl;
+            }
+        } while (filas < 2 || filas > 20);
+        do {
+            cout << "Columnas (2-20): " << endl;
+            cin >> columnas;
+            if (columnas < 2 || columnas > 20) {
+                cout << "Valor invalido. Las columnas deben estar entre 2 y 20." << endl;
+            }
+        } while (columnas < 2 || columnas > 20);
+        int matriz[20][20];
+        do {
+            cout << "Ingrese las dimensiones de la matriz_2 deseada: " << endl;
+            cout << "Filas (2-20): " << endl;
+            cin >> filas2;
+            if (filas2 < 2 || filas2 > 20) {
+                cout << "Valor invalido. Las filas deben estar entre 2 y 20." << endl;
+            }
+        } 
+        while (filas2 < 2 || filas2 > 20);
+        do {
+            cout << "Columnas (2-20): " << endl;
+            cin >> columnas2;
+            if (columnas2 < 2 || columnas2 > 20) {
+                cout << "Valor invalido. Las columnas deben estar entre 2 y 20." << endl;
+            }
+            } 
+        while (columnas2 < 2 || columnas2 > 20);
 
-        cout << "Ingrese las dimensiones de la matriz_2 desada: " << endl;
-        cout <<"Filas: " << endl;
-        cin >> filas2;
-        cout << "" << endl;
-        cout << "Columnas: " << endl;
-        cin >> columnas2;
-        cout << "" << endl;
-        int matriz2[filas2][columnas2];
-        int matrizres[filas][columnas];
-    // Menú de opciones
-    do{
-        menu();
-        switch (opcion) 
-        {
-            case 1:
-                crearMatriz(matriz,filas,columnas,matriz2);
-                break;
-            case 2:
-                sumar_matriz(matriz,matriz2,matrizres,filas,columnas);
-                break;
-            case 3:
-                reflexiva(matriz,matriz2,filas,columnas);
-                break;
-            case 4:
-                cout << "Reiniciar Matrices" << endl;
-                reiniciar = true;
-                main();
-                break;
-            case 5:
-                cout << "Se salio del programa" << endl;
-                reiniciar = true;
-                break;
-            default:
-                cout << "Entrada Invalida porfavor ingrese el numero correspondiente a la opcion deseada";
-                menu();
-                break;
-            return 0;
-        }
-    }while (opcion != 5);
-    }
+
+        int matriz2[20][20];
+        int matrizres[20][20];
+
+        do {
+            menu();
+            switch (opcion) 
+                {
+                case 1:
+                    // Crear dos matrices
+                    crearMatriz(matriz, filas, columnas, matriz2);
+                    bandera = true;
+                    break;
+
+                case 2:
+                    cout << "Reiniciar Matrices" << endl;
+                    reiniciar = true;
+                    main();
+                    return 0;
+                    break;
+                case 3:
+                    cout << "Se salio del programa" << endl;
+                    return 0;
+                    break;
+                default:
+                    cout << "Entrada Invalida, por favor ingrese el numero correspondiente a la opcion deseada." << endl;
+                    menu();
+                    return 0;
+                    break;
+                }
+            } while (bandera == false);
+        do
+            {
+                menu2();
+                switch (opcion)
+                {
+                    case 1:
+                        sumar_matriz(matriz, matriz2, matrizres, filas, columnas);
+                        break;
+                        case 2:
+                            reflexiva(matriz, matriz2, filas, columnas);
+                            break;
+                        case 3:
+                            cout << "Reiniciar Matrices" << endl;
+                           
+                            main();
+                            break;
+                        case 4:
+                            cout << "Se salio" << endl;
+                            bandera = false;
+                            break;
+                        default: 
+                            cout << "Entrada Invalida, por favor ingrese el numero correspondiente a la opcion deseada." << endl;
+                                menu2();
+                            break;
+                }
+            }while (bandera);
+    //}
+    return 0;
 }
